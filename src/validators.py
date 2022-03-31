2f3997ce0afe5fc5759ec8d8d06277960c5b9e8d
yu-cherabai <yurii.cherabai@altexsoft.com>
2022-03-31 19:28:49 +0200

    updated

--- src/validators.py
+++ src/validators.py
@@ -14,10 +14,10 @@ def is_separate_request_valid(request: SeparateRequest) -> bool:
         return False
 
     file_ext = path_parts[1].split('.', 1)[1]
-    if file_ext not in CodecsIn.__members__.values():
+    if file_ext not in [item.value for item in CodecsIn]:
         return False
 
-    if file_ext != request.inputSoundFormat:
+    if file_ext != request.inputSoundFormat.value:
         return False
 
     return True
