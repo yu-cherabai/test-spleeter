22cafda029ca75c94d0a92ce7d3db6d962db4b84
yu-cherabai <yurii.cherabai@altexsoft.com>
2022-04-17 20:59:27 +0200

    updated

--- src/celery_tasks.py
+++ src/celery_tasks.py
@@ -1,16 +1,8 @@
-import logging
-
-import requests
-
 from celery import Celery
 from celery.signals import worker_init
 from pydantic import parse_raw_as
 
-from src.constants import FOLDER_PROCESSING, WEBHOOK_HOST
-from src.gcp import download_file, upload_file
 from src.models import SeparateRequest
-from src.separator import separate_by_chunks
-from src.utils import create_request_folder, delete_request_folder
 
 
 def get_celery() -> Celery:
@@ -27,36 +19,7 @@ celery = get_celery()
 )
 def process(request_json: str):
     request: SeparateRequest = parse_raw_as(SeparateRequest, request_json)
-    request_folder = f'{FOLDER_PROCESSING}/{request.id}'
-    downloaded_file_path = f'{request_folder}/{request.id}.{request.input_sound_format.value}'
-
-    create_request_folder(request_folder)
-    logging.info('Start downloading a file from the bucket.')
-    download_file(request.path, downloaded_file_path)
-    logging.info('File downloaded from the bucket.')
-
-    separate_by_chunks(
-        downloaded_file_path, request.id, request_folder, request.input_sound_format.value, request.output_sound_format.value)
-
-    logging.info('Start uploading results of separation.')
-    upload_file(
-        f'{request_folder}/speach.{request.output_sound_format.value}',
-        f'results/{request.id}/speach.{request.output_sound_format.value}')
-    upload_file(
-        f'{request_folder}/background.{request.output_sound_format.value}',
-        f'results/{request.id}/background.{request.output_sound_format.value}'
-    )
-    logging.info('Results uploaded.')
-
-    send_notification(request.id)
-
-    delete_request_folder(request_folder)
-
-
-def send_notification(request_id):
-    if WEBHOOK_HOST:
-        logging.info('Send notification of successful file separation.')
-        requests.post(f'{WEBHOOK_HOST}/api/v1/orders/{request_id}/audio_split_finished')
+    print('123')
 
 
 def restore_all_unacknowledged_messages():
